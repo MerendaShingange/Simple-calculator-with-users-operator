7cab988a2bc4ec11a1e022b71324a337f147472b
Lenovo-User <Lenovo-User@DESKTOP-TIU29D7>
2024-01-23 08:39:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_calculator_with_users_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter an operator");
+            string op = Console.ReadLine();
+            Console.WriteLine("enter a second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+
+
+
+
+            }
+            else if (op == "/")
+            { 
+                Console.WriteLine(num1 / num2);
+
+            }
+            else
+            {
+                Console.WriteLine("not an operator");
+            }
+            Console.Read();
+        }
+    }
+}
